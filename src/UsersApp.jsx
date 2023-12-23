b700dc999e3ad929b1a4d8207ee273efa6ae32fc
vitochi <vitaro.jmc@gmail.com>
2023-12-23 17:12:25 -0300

    uso de la funcion fetch desde el inicio

--- src/UsersApp.jsx
+++ src/UsersApp.jsx
@@ -1,27 +1,29 @@
-import { useState } from "react"
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 export const UsersApp = () => {
 
-const [users, setUsers] = useState([])
+  const [users, setUsers] = useState([])
 
-const fetchUsers = async() => {
-  try{
-    const response = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = response.json()
-    setUsers(data)
-  }catch{
-    console.error(error)
+  const fetchUsers = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/users')
+      const data = await response.json()
+      setUsers(data)
+    } catch {
+      console.error(error)
+    }
   }
-}
+
+  useEffect ( () => {
+    fetchUsers()
+  }, [])
 
   return (
     <>
-    <h1>Lista de Usuarios:</h1>
-    <ul>
-      <li>usuario uno</li>
-      <li>usuario dos</li>
-    </ul>
+      <h1>Lista de Usuarios:</h1>
+      <ul>
+        {users.map(user => <li key={user.id}>Nombre: {user.name} | Email: {user.email}</li>)}
+      </ul>
     </>
   )
 }
